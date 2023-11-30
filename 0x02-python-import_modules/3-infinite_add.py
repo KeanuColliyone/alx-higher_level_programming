a9431f5fef27ec21cc60440fd2c8b6bf5c8db1db
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 15:51:47 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-total = sum(int(arg) for arg in arguments)
-print(total)
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    total = sum(int(arg) for arg in arguments)
+    print(total)
