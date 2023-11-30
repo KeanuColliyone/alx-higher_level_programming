e3c7b8068ab9e138a142557a67825aad0880ddce
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 15:49:03 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-num_args = len(argv) - 1
-if num_args == 0:
-    print("0 arguments.")
-else:
-    if num_args == 1:
-        print("1 argument:")
+if __name__ == "__main__":
+    from sys import argv
+    num_args = len(argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
     else:
-        print(f"{num_args} arguments:")
-    for i in range(1, num_args + 1):
-        print(f"{i}: {argv[i]}")
+        if num_args == 1:
+            print("1 argument:")
+        else:
+            print(f"{num_args} arguments:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {argv[i]}")
