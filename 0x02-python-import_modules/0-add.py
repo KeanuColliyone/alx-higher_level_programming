362ebb7059d648326b6e56954f34ac08558f4ae8
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 15:45:24 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the addition of numbers  1 and 2 respectively."""
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
