88079832c006686f7b12c016843c1d55bead819a
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 15:53:32 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import types
-with open('hidden_4.pyc', 'rb') as file:
-    code = file.read()
-module = types.CodeType.co_consts[0]
-names = [name.decode() for name in module.co_names if not name.startswith('__')]
-names.sort()
-for name in names:
-    print(name)
+if __name__ == "__main__":
+    import types
+    with open('hidden_4.pyc', 'rb') as file:
+        code = file.read()
+    module = types.CodeType.co_consts[0]
+    names = [name.decode() for name in module.co_names if not name.startswith('__')]
+    names.sort()
+    for name in names:
+        print(name)
