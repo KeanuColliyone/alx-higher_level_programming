452757d51805d90b133568f28042d613de2c1911
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 18:13:34 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import importlib.util
 if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", file_name)
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
     file_name = 'hidden_4.pyc'
     names = dir(hidden_4)
     for name in names:
