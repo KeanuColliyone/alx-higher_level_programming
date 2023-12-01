1f1ff4a99381a33b9b992ce673363de4bc4a412f
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-30 19:03:22 -0800

    tell me why

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+import dis
+import types
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    with open('hidden_4.pyc', 'rb') as file:
+    code = file.read()
+    module = types.CodeType.from_codelist(code)
+    names = set()
+    for instruction in dis.get_instructions(module):
+        if instruction.opname == 'LOAD_NAME':
+            name = instruction.argval
+            if not name.startswith('__'):
+                names.add(name)
+    for name in sorted(names):
+        print(name)
