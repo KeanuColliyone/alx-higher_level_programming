c038925cbcd1f43c247629ed5a412f359421afeb
Kenan Kotzee <frostcolliyone@gmail.com>
2023-12-06 11:25:44 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_value = max(a_dictionary.values())
-        best_key = [key for key, value in a_dictionary.items() if value == max_value][0]
+        best_key = [key for key, value in a_dictionary.items()
+                if value == max_value][0]
         return best_key
     return None
