7efa4e71af90fc06c008bfd627aff85cb4a819e2
Kenan Kotzee <frostcolliyone@gmail.com>
2023-12-19 12:51:18 -0800

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a Square class representing a geometric square."""
 
+
 class Square:
     """This class represents a square with methods and attributes."""
     def __init__(self):
