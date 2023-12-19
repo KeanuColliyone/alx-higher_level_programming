0f587bc44c31be795ec0eeff196be9129b60ce7a
Kenan Kotzee <frostcolliyone@gmail.com>
2023-12-18 16:52:38 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,29 +3,39 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            val_1 = float(my_list_1[i])
-                    if isinstance(my_list_1[i], (int, float))
-                    else "wrong type"
-            val_2 = float(my_list_2[i])
-                    if isinstance(my_list_2[i], (int, float))
-                    else "wrong type"
+            val_1 = my_list_1[i] if i < len(my_list_1) else 0
+            val_2 = my_list_2[i] if i < len(my_list_2) else 0
 
-            try:
-                division = val_1 / val_2 if val_2 != 0 else "division by 0"
-            except ZeroDivisionError:
-                division = "division by 0"
-
-        except IndexError:
-            division = "out of range"
-
-        finally:
-            if division == "wrong type":
-                print("wrong type")
-            elif division == "division by 0":
-                print("division by 0")
-            elif division == "out of range":
-                print("out of range")
+            if isinstance(val_1, (int, float)) and isinstance(val_2, (int, float)):
+                if val_2 == 0:
+                    raise ZeroDivisionError("division by 0")
+                result.append(val_1 / val_2)
             else:
-                result.append(division)
+                raise TypeError("wrong type")
+
+        except ZeroDivisionError as e:
+            print(e)
+            result.append(0)
+        except TypeError as e:
+            print(e)
+            result.append(0)
+        except IndexError as e:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass
 
     return result
+
+# Test cases
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
