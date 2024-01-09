277f239f73ad3d4f060c92261c7bfc2e71822a4d
Kenan Kotzee <frostcolliyone@gmail.com>
2024-01-09 07:59:45 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry"""
+
+
 class BaseGeometry:
     """Class representing a base geometry."""
 
     def area(self):
         """Raises an Exception indicating that area() is not implemented."""
         raise Exception("area() is not implemented")
+
 
     def integer_validator(self, name, value):
         """Validates the value and raises exceptions if conditions are not met."""
