4dd2caaaddd1a1b6f472b0e48eefa8ef62db4d5b
Kenan Kotzee <frostcolliyone@gmail.com>
2024-03-28 17:16:42 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,25 +1,4 @@
--- Check and list privileges for 'user_0d_1'@'localhost'
-SELECT CONCAT('User user_0d_1 exists with privileges: ', GROUP_CONCAT(privilege_type))
-FROM information_schema.user_privileges
-WHERE grantee = '\'user_0d_1\'@\'localhost\''
-GROUP BY grantee
-UNION
-SELECT 'User user_0d_1 does not exist or has no privileges'
-WHERE NOT EXISTS (
-    SELECT *
-    FROM information_schema.user_privileges
-    WHERE grantee = '\'user_0d_1\'@\'localhost\''
-);
-
--- Check and list privileges for 'user_0d_2'@'localhost'
-SELECT CONCAT('User user_0d_2 exists with privileges: ', GROUP_CONCAT(privilege_type))
-FROM information_schema.user_privileges
-WHERE grantee = '\'user_0d_2\'@\'localhost\''
-GROUP BY grantee
-UNION
-SELECT 'User user_0d_2 does not exist or has no privileges'
-WHERE NOT EXISTS (
-    SELECT *
-    FROM information_schema.user_privileges
-    WHERE grantee = '\'user_0d_2\'@\'localhost\''
-);
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,20 @@
--- Attempt to set or update the user's password
-SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
--- Grant all privileges to the user, creating the user if it doesn't exist
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
--- Apply changes immediately
+-- Check if the user exists and create if it does not
+DELIMITER $$
+
+CREATE PROCEDURE EnsureUserExists()
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1') THEN
+    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+  END IF;
+END$$
+
+DELIMITER ;
+
+CALL EnsureUserExists();
+DROP PROCEDURE IF EXISTS EnsureUserExists;
+
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure the changes take effect
 FLUSH PRIVILEGES;
