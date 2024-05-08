76234fd527ef191349b60a6cd6a13b78a00f9b96
KeanuColliyone <139171593+KeanuColliyone@users.noreply.github.com>
2024-05-08 22:08:47 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => {
-    return count + (current === searchElement ? 1 : 0);
-  }, 0);
-};
